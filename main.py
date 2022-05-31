5b9a2aea64b1d94256f2bb278b5a6b39bc78dd6d
Sephens <adesephens@gmail.com>
2022-05-31 22:51:00 +0300

    Added a loop to enable play again

--- main.py
+++ main.py
@@ -10,33 +10,41 @@ import random
 # Play several games in a row using a while loop
 # Clean up your code with Enum and functions
 # Define more complex rules with a dictionary
+# Display a welcome message
+print("Welcome to Rock-Paper-Scissors game")
+while True:
+    #Take User Input
+    user_action = input("Enter a choice ('R-rock' , 'S-Scissor', 'P-Paper'): ")
 
-# Take User Input
-user_action = input("Enter a choice ('R-rock' , 'S-Scissor', 'P-Paper'): ")
+    # Make the Computer Choose
+    # You can use random.choice() to have the computer randomly select between the actions:
+    possible_action = ["R", "P", "S"]
+    computer_action = random.choice(possible_action)
 
-# Make the Computer Choose
-# You can use random.choice() to have the computer randomly select between the actions:
-possible_action = ["R", "P", "S"]
-computer_action = random.choice(possible_action)
+    # You can also print the choices that the user and the computer made:
+    print("\n You chose {}, computer chose {} \n".format(user_action,computer_action))
 
-# You can also print the choices that the user and the computer made:
-print("\n You chose {}, computer chose {} \n".format(user_action,computer_action))
-
-# Determine a Winner
-if computer_action == user_action:
-    print("Both of you chose {} and it is a tie".format(user_action))
-elif user_action == "R":
-    if computer_action == "S":
-        print("Rock smashes scissors! You win!")
+    # Determine a Winner
+    if computer_action == user_action:
+        print("Both of you chose {} and it is a tie".format(user_action))
+    elif user_action == "R":
+        if computer_action == "S":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers Rock. You lose!")
+    elif user_action == "P":
+        if computer_action == "R":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "S":
+        if computer_action == "P":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
     else:
-        print("Paper covers Rock. You lose!")
-elif user_action == "P":
-    if computer_action == "R":
-        print("Paper covers rock! You win!")
-    else:
-        print("Scissors cuts paper! You lose.")
-elif user_action == "S":
-    if computer_action == "P":
-        print("Scissors cuts paper! You win!")
-    else:
-        print("Rock smashes scissors! You lose.")
+        print("Error! You made a wrong selection")
+
+    play_again = input("Play again? (Y/N)")
+    if play_again != "y":
+        break
